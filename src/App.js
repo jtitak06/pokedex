606b6ed8833bf3265b02745979bf29289cf96a3b
Jeff Titak <jtitak06@yahoo.com>
2022-01-18 15:15:55 +0300

    Attempted to fix bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Pokedex from "./Pokedex.js";
 import Pokemon from "./Pokemon.js";
 
 function App() {
+  const location = useLocation();
   return (
     <BrowserRouter>
-      <Routes>
+      <Routes location={location}>
         <Route path="/" element={<Pokedex />} />
         <Route path="/:pokemonId" element={<Pokemon />} />
       </Routes>
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Pokedex = () => {
+const Pokedex = (props) => {
     return(
         <div>
             Pokedex Page
